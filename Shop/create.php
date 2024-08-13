cb18d35b5a11f8bbe6240348bb2fe36032623c1a
SunaliB <147754754+Basnayake07@users.noreply.github.com>
2024-08-13 18:57:55 +0530

    Add files via upload

---
+++ Shop/create.php
@@ -0,0 +1,121 @@
+<?php
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$m_name = $address = $s_email = "";
+$mname_err = $address_err = $semail_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    // Validate name
+    $input_name = trim($_POST["name"]);
+    if(empty($input_name)){
+        $mname_err = "Please enter a name.";
+    } elseif(!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $mname_err = "Please enter a valid name.";
+    } else{
+        $m_name = $input_name;
+    }
+
+    // Validate location
+    $input_address = trim($_POST["address"]);
+    if(empty($input_address)){
+        $address_err = "Please enter an address.";     
+    } else{
+        $address = $input_address;
+    }
+    
+   // Validate email
+    $input_email = trim($_POST["email"]);
+    if (empty($input_email)) {
+        $semail_err = "Fill the email address!<br />";
+    } else {
+    // Check if email contains any uppercase letters
+        if (preg_match('/[A-Z]/', $input_email)) {
+        $semail_err = "This email address must contain only lowercase letters.";
+        } elseif (!filter_var($input_email, FILTER_VALIDATE_EMAIL)) {
+        $semail_err = "This email address is invalid.";
+        } else {
+        $s_email = $input_email;
+        }
+    }
+    
+    // Check input errors before inserting in database
+    if(empty($mname_err) && empty($address_err) && empty($semail_err)){
+        // Prepare an insert statement
+        $sql = "INSERT INTO shop (man_name, address, semail) VALUES (?, ?, ?)";
+ 
+        if($stmt = $mysqli->prepare($sql)){
+            // Bind variables to the prepared statement as parameters
+            $stmt->bind_param("sss", $param_name, $param_address, $param_email);
+            
+            // Set parameters
+            $param_name = $m_name;
+            $param_address = $address;
+            $param_email = $s_email;
+            
+            // Attempt to execute the prepared statement
+            if($stmt->execute()){
+                // Records created successfully. Redirect to landing page
+                header("location: index.php");
+                exit();
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+         
+        // Close statement
+        $stmt->close();
+    }
+    
+    // Close connection
+    $mysqli->close();
+}
+?>
+ 
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Create Record</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        .wrapper{
+            width: 600px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="mt-5">Create Record</h2>
+                    <p>Please fill this form and submit to add shop record to the database.</p>
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="form-group">
+                            <label>Manager's Name</label>
+                            <input type="text" name="name" class="form-control <?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $m_name; ?>">
+                            <span class="invalid-feedback"><?php echo $mname_err;?></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Address</label>
+                            <textarea name="address" class="form-control <?php echo (!empty($address_err)) ? 'is-invalid' : ''; ?>"><?php echo $address; ?></textarea>
+                            <span class="invalid-feedback"><?php echo $address_err;?></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Shop's Email</label>
+                            <input type="text" name="email" class="form-control <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $s_email; ?>">
+                            <span class="invalid-feedback"><?php echo $email_err;?></span>
+                        </div>
+                        <input type="submit" class="btn btn-primary" value="Submit">
+                        <a href="index.php" class="btn btn-secondary ml-2">Cancel</a>
+                    </form>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
