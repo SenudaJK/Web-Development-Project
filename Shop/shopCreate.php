134f70979b6619171fcb4835c33af5dbcf155d41
SunaliB <147754754+Basnayake07@users.noreply.github.com>
2024-08-14 21:53:15 +0530

    Update and rename create.php to shopCreate.php

--- Shop/shopCreate.php
+++ Shop/shopCreate.php
@@ -1,6 +1,6 @@
 <?php
 // Include config file
-require_once "config.php";
+require_once "ShopConfig.php";
  
 // Define variables and initialize with empty values
 $m_name = $address = $s_email = "";
@@ -58,7 +58,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             // Attempt to execute the prepared statement
             if($stmt->execute()){
                 // Records created successfully. Redirect to landing page
-                header("location: index.php");
+                header("location: shopIndex.php");
                 exit();
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
