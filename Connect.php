2cfbd5d552663ab6db59b810ca0499d713059e30
Dhanitha Kolonnage <152945350+dhanitha0725@users.noreply.github.com>
2024-08-23 10:13:25 +0530

    Delete Connect.php

--- Connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = new mysqli('localhost', 'root', '', 'inventorydb');
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error . " (Error No: " . $conn->connect_errno . ")");
-}
