28339f6ad07819764652e855ee7a65b9f4336618
SunaliB <147754754+Basnayake07@users.noreply.github.com>
2024-08-24 14:36:28 +0530

    Update purchaseUpdate.php

--- purchaseUpdate.php
+++ purchaseUpdate.php
@@ -235,7 +235,7 @@ if (isset($_POST["id"]) && !empty(trim($_POST["id"]))) {
                             <a class="nav-link active" href="dashboard.php"><i class="material-icons">home</i>Dashboard</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="InventoryUpdate.php"><i class="material-icons">inventory</i>inventory</a>
+                            <a class="nav-link" href="InventoryUpdate.php"><i class="material-icons">inventory</i>Inventory</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="productGet.php"><i class="material-icons">category</i>Products</a>
@@ -244,7 +244,7 @@ if (isset($_POST["id"]) && !empty(trim($_POST["id"]))) {
                             <a class="nav-link" href="purchaseView.php"><i class="material-icons">shopping_cart</i>Purchase Orders</a>                            
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#"><i class="material-icons">sell</i>Dispatch Orders</a>                            
+                            <a class="nav-link" href="dispatchedOrders.php"><i class="material-icons">sell</i>Dispatch Orders</a>                            
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="suppliers.php"><i class="material-icons">local_shipping</i>Suppliers</a>
